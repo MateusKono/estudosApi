4451a190f25f3cbfa58384bd5e6c72a381776124
Mateus Kono <mkono@ciandt.com>
2016-06-07 09:17:20 -0300

    Tratamento correto das respostas HTTP 404 (Not found) e 201 (Created)/Finalizando o tratamento das respostas

--- src/main/java/br/com/estudoapi/resource/LivrosResource.java
+++ src/main/java/br/com/estudoapi/resource/LivrosResource.java
@@ -1,14 +1,19 @@
 package br.com.estudoapi.resource;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.estudoapi.pojo.Livro;
 import br.com.estudoapi.repository.LivrosRepository;
@@ -19,31 +24,46 @@ public class LivrosResource {
 	
 	@Autowired
 	private LivrosRepository livroRepository;
-	
+		
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar(){
-		return livroRepository.findAll();
+	public ResponseEntity<List<Livro>> listar(){
+		return ResponseEntity.ok(livroRepository.findAll());
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro){
-		livroRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro){
+		livro = livroRepository.save(livro);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(livro.getId()).toUri();
+				
+		return ResponseEntity.created(location).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable(value = "id") long id){
-		return livroRepository.findById(id);
+	public ResponseEntity<Livro> buscar(@PathVariable(value = "id") long id){
+		if(livroRepository.findById(id) == null)
+			return new ResponseEntity<Livro>(HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<Livro>(livroRepository.findById(id), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void remover(@PathVariable(value = "id") long id){
-		Livro livro = livroRepository.findById(id);
-		livroRepository.delete(livro);
+	public ResponseEntity<Void> remover(@PathVariable(value = "id") long id){
+		livroRepository.delete(livroRepository.findById(id));
+		
+		return ResponseEntity.ok().build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar(@RequestBody Livro livro, @PathVariable(value = "id") long id){
+	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable(value = "id") long id){
 		livro.setId(id);
+		
 		livroRepository.save(livro);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("").
+				buildAndExpand(livro.getId()).toUri();;
+				
+		return ResponseEntity.created(location).build();
 	}
 }
